caa3fa67d7239a495350fb8bd2f09303c2e33037
Дарья Колбасина <kolbasina9a@gmail.com>
2021-10-21 10:34:56 +0300

    Add IsleapYear

--- DateTime.pas
+++ DateTime.pas
@@ -1,4 +1,22 @@
 ﻿uses system;
 
 
-function
+function IsLeapYear(x:integer):boolean;
+begin
+  if x mod 4 =0 then
+  begin
+    if x mod 100<>0 then
+    begin
+      result:=false;
+    end
+    else
+    begin
+      result:=true;
+    end;
+  end;
+end;
+
+begin
+  print('Год високосный');
+  print(IsLeapYear(2000));
+end.
